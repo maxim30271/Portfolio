7496bad64acf865a55a13ead03f03e3e7261b841
maxim30271 <45605144+maxim30271@users.noreply.github.com>
2019-04-16 23:44:22 +0300

    initial commit

---
+++ My_book_store/dist/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".my-rating").starRating({
+        initialRating: 4,
+        strokeColor: '#894As00',
+        strokeWidth: 10,
+        starSize: 25
+      });
+});
